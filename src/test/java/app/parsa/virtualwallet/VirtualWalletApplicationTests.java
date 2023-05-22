6873c8cf04b2c7a3b70a6c03d36b261eebd23823
parsa <panahandehparsa@gmail.com>
2023-05-22 15:47:49 +0330

    Initial commit

---
+++ src/test/java/app/parsa/virtualwallet/VirtualWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package app.parsa.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
